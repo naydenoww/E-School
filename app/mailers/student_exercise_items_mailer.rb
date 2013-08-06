135d29380c8fbb7a05092082d46c5000c2d9ca76
Borislav <naydenoww@mail.bg>
2013-08-06 09:39:34 +0300

    Changed method. welcome_email now is results_email

--- app/mailers/student_exercise_items_mailer.rb
+++ app/mailers/student_exercise_items_mailer.rb
@@ -1,7 +1,7 @@
 class StudentExerciseItemsMailer < ActionMailer::Base
   default from: "from@example.com"
 
-  def welcome_email
+  def results_email
     mail(:to => "naydenoww@mail.bg", :subject => "Test mail Ruby", :body => "Test mail bodyxaxaxaxaxa")
   end
 end
