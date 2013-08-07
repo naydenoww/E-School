d5f0eb54db8ab81a75310dc26a2d57fde0df3db9
Borislav <naydenoww@mail.bg>
2013-08-07 16:45:21 +0300

    Removed email body

--- app/mailers/student_exercise_items_mailer.rb
+++ app/mailers/student_exercise_items_mailer.rb
@@ -2,6 +2,6 @@ class StudentExerciseItemsMailer < ActionMailer::Base
   default from: "from@example.com"
 
   def results_email
-    mail(:to => "naydenoww@mail.bg", :subject => "Test mail Ruby", :body => "Test mail bodyxaxaxaxaxa")
+    mail(:to => "naydenoww@mail.bg", :subject => "Test mail Ruby")
   end
 end
